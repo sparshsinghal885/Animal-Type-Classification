82c70e85aa318b91591a8c49cedb09d430ba01a5
sparshsinghal885 <sparshsinghal885@gmail.com>
2025-09-13 20:45:58 +0530

    get mapping -> post mapping

--- src/main/java/com/ninja/AnimalTypeClassification/controller/AIImageClassification.java
+++ src/main/java/com/ninja/AnimalTypeClassification/controller/AIImageClassification.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -28,7 +25,7 @@ public class AIImageClassification {
     @Value("${api.key}")
     private String apiKey;
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<List<ClassificationDetailDto>> getAnimalDetails(@RequestParam("images") MultipartFile image){
         String imageUrl;
         String path = "animal/extra";
